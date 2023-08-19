c4433ff414ca2e0800d99092d4010d9c6670d4f3
Kurapika <victorakhihiero@gmail.com>
2023-08-19 12:51:51 +0100

    fix

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,8 +1,2 @@
--- List all the databases in my server
-SHOW databases;
-
---this creates a new database only if it doesn't already exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
---this deletes the named database only if it exists
+-- this deletes the named database only if it exists
 DROP DATABASE IF EXISTS hbtn_0c_0;
