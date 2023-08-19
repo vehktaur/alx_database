5e41a069d41e78782361864c9ae1c2a43299c7bf
Kurapika <victorakhihiero@gmail.com>
2023-08-19 12:36:35 +0100

    fixes

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,3 +1,3 @@
 -- List all the databases in my server
-SHOW databases
+SHOW databases;
 
--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 --this creates a new database only if it doesn't already exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
