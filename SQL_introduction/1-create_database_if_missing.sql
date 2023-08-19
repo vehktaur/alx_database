fa4b956806fd3638f37d77e77a7dfe4569b94872
Kurapika <victorakhihiero@gmail.com>
2023-08-19 12:23:29 +0100

    update

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---thIS creates a new database only if it doesn't already exist
+--this creates a new database only if it doesn't already exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+--this deletes the named database only if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0
