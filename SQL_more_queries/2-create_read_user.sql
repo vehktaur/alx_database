eb622d480671979ad862cc22a9013965adf92d18
Kurapika <victorakhihiero@gmail.com>
2023-08-20 16:18:05 +0100

    update

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,1 +1,9 @@
 -- create a new USER
+
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+GRANT SELECT ON hbtn_0d_2.* TO "user_0d_1"@"localhost" ;
+
+FLUSH PRIVILEGES;
