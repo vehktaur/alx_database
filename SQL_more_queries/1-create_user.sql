166be009ae78ea12dc25ba928588a08fe1489d2c
Kurapika <victorakhihiero@gmail.com>
2023-08-20 16:14:41 +0100

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
 -- create a new mysql USER
 
-CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
 
+GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost" ;
+
+FLUSH PRIVILEGES;
