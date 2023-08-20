05baa6c8cf97d77af7d290d088217014b2810225
Kurapika <victorakhihiero@gmail.com>
2023-08-20 17:03:17 +0100

    fix

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all the cities in the DATABASE
 
 SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN cities ON cities.state_id = states.id
+LEFT JOIN cities ON states.id = cities.state_id 
 ORDER BY cities.id;
