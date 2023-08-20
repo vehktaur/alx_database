95a0f4144e7f911c21cf029e9bf840f12a6d216b
Kurapika <victorakhihiero@gmail.com>
2023-08-20 16:20:31 +0100

    check

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 -- create a new USER
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 GRANT SELECT ON hbtn_0d_2.* TO "user_0d_1"@"localhost" ;
 
