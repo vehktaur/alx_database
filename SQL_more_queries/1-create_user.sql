e35363d50b938602ac20e53e0eccae8e43da19c1
Kurapika <victorakhihiero@gmail.com>
2023-08-20 16:12:06 +0100

    check

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- create a new mysql USER
 
-CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY user_0d_1_pwd;
+
+SET PASSWORD FOR ""
