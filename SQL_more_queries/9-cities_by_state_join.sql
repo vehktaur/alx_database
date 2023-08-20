a711fa796000700f9b6a649dfd08fe91cb193c49
Kurapika <victorakhihiero@gmail.com>
2023-08-20 16:59:18 +0100

    update

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,5 @@
--- list all the
+-- list all the cities in the DATABASE
+
+SELECT cities.id, cities.name, states.name FROM cities
+LEFT JOIN cities ON cities.state_id = states.id
+ORDER BY cities.id;
